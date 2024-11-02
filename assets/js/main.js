060568570a369a043ec07cf6e0b4fe5fcf4bfd0f
Muhye aldeen <86913045+MuhyeAlRaie@users.noreply.github.com>
2024-11-02 18:32:20 +0300

    add topbar for social media

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,14 @@ window.addEventListener('scroll', function () {
     }
 });
 
+// Add event listener for scroll
+window.addEventListener('scroll', function () {
+    const topbar = document.querySelector('.topbar');
+    
+    // Check if page is scrolled more than 50px
+    if (window.scrollY > 50) {
+        topbar.classList.add('scrolled');
+    } else {
+        topbar.classList.remove('scrolled');
+    }
+});
