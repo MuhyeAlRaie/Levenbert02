e9473bfffe06d5fbb96a1bc94e1086ea4b1d0a46
Muhye aldeen <86913045+MuhyeAlRaie@users.noreply.github.com>
2024-11-06 03:00:47 +0300

    add scroll top btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,29 @@ window.addEventListener('scroll', function () {
     }
 });
 
+/**
+   * Scroll top button
+   */
+let scrollTop = document.querySelector('.scroll-top');
+
+function toggleScrollTop() {
+  if (scrollTop) {
+    window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+  }
+}
+scrollTop.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+window.addEventListener('load', toggleScrollTop);
+document.addEventListener('scroll', toggleScrollTop);
+
+// scroll timeline in overview
+
 document.addEventListener("DOMContentLoaded", function() {
   gsap.registerPlugin(ScrollTrigger);
 
